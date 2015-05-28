5c6ad9d6b4c4b5f0e0a6b5da2523f4da69b90d23
jmedina2099 <medinarosas.jorge@gmail.com>
2015-05-28 11:14:46 -0500

    Fuentes cambios
    Fuentes cambios

---
+++ src/org/hashuniforme/hash/funciones/HashIteradaRandomBits.java
@@ -0,0 +1,224 @@
+package org.hashuniforme.hash.funciones;
+
+
+public class HashIteradaRandomBits implements FuncionHash {
+
+	private static int PRIME = 101;
+
+	public HashIteradaRandomBits() {
+	}
+	
+	public HashIteradaRandomBits( int prime ) {
+		PRIME = prime;
+	}
+
+	@Override
+	public int getHash(String o) {
+		int hash = 0;
+		try {
+			hash += getHashEval( o);
+			//hash += o.hashCode();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if( hash < 0 ) {
+			hash = Integer.MAX_VALUE + hash;
+		}
+		
+		return hash % PRIME;
+	}
+	
+
+	public int getHashEval( String o) {
+
+		int hash = 0;
+		int char1,char2,char3,char4,char5;
+
+		char1 = (int)o.charAt( o.length()-2 );
+		char2 = (int)o.charAt( o.length()-1 );
+		char3 = (int)o.charAt( 0 );
+		char4 = (int)o.charAt( 1 );
+		char5 = (int)o.charAt( 2 );
+		hash += evaluaFuncBool( char1,char2,char3,char4,char5);
+
+		char1 = (int)o.charAt( o.length()-1 );
+		char2 = (int)o.charAt( 0 );
+		char3 = (int)o.charAt( 1 );
+		char4 = (int)o.charAt( 2 );
+		char5 = (int)o.charAt( 3 );
+		hash += evaluaFuncBool( char1,char2,char3,char4,char5);
+
+		// Main Loop.
+		for( int i=2; i<o.length()-2; i++ ) {
+			char1 = (int)o.charAt( i-2 );
+			char2 = (int)o.charAt( i-1 );
+			char3 = (int)o.charAt( i );
+			char4 = (int)o.charAt( i+1 );
+			char5 = (int)o.charAt( i+2 );
+			hash += evaluaFuncBool( char1,char2,char3,char4,char5);
+		}
+
+		char1 = (int)o.charAt( o.length()-4 );
+		char2 = (int)o.charAt( o.length()-3 );
+		char3 = (int)o.charAt( o.length()-2 );
+		char4 = (int)o.charAt( o.length()-1 );
+		char5 = (int)o.charAt( 0 );
+		hash += evaluaFuncBool( char1,char2,char3,char4,char5);
+
+		char1 = (int)o.charAt( o.length()-3 );
+		char2 = (int)o.charAt( o.length()-2 );
+		char3 = (int)o.charAt( o.length()-1 );
+		char4 = (int)o.charAt( 0 );
+		char5 = (int)o.charAt( 1 );
+		hash += evaluaFuncBool( char1,char2,char3,char4,char5);
+
+		
+		if( hash < 0 ) {
+			hash = Integer.MAX_VALUE + hash;			
+		}
+
+		return hash %=	PRIME;	
+	}
+	
+	public int evaluaFuncBool(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		int hash = evaluaFuncBool1( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool2( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool3( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool4( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool5( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool6( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool7( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool8( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool9( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool10( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool11( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool12( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool13( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool14( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool15( char1,char2,char3,char4,char5 ) +
+				   evaluaFuncBool16( char1,char2,char3,char4,char5 );
+		
+		return hash;	
+	}
+	
+	// (3,0,0,1)=(|,+,+,^)
+	public int evaluaFuncBool1(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 | char2 ) + ( char3 + char4 ) ^ char5;
+	}
+
+	// (0,3,0,0)=(+,|,+,+)
+	public int evaluaFuncBool2(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) | ( char3 + char4 ) + char5;
+	}
+
+	// (0,1,0,1)=(+,^,+,^)
+	public int evaluaFuncBool3(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) ^ ( char3 + char4 ) ^ char5;
+	}
+
+	// (0,3,0,1)=(+,|,+,^)
+	public int evaluaFuncBool4(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) | ( char3 + char4 ) ^ char5;
+	}
+
+	// (2,0,0,1)=(&,+,+,^)
+	public int evaluaFuncBool5(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 & char2 ) + ( char3 + char4 ) ^ char5;
+	}
+
+	// (1,0,1,0)=(^,+,^,+)
+	public int evaluaFuncBool6(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 ^ char2 ) + ( char3 ^ char4 ) + char5;
+	}
+
+	// (1,1,0,1)=(^,^,+,^)
+	public int evaluaFuncBool7(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 ^ char2 ) ^ ( char3 + char4 ) ^ char5;
+	}
+
+	// (0,0,0,1)=(+,+,+,^)
+	public int evaluaFuncBool8(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) + ( char3 + char4 ) ^ char5;
+	}
+
+	// (1,0,0,1)=(^,+,+,^)
+	public int evaluaFuncBool9(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 ^ char2 ) + ( char3 + char4 ) ^ char5;
+	}
+
+	// (1,0,3,0)=(^,+,|,+)
+	public int evaluaFuncBool10(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 ^ char2 ) + ( char3 | char4 ) + char5;
+	}
+
+	// (3,1,0,1)=(|,^,+,^)
+	public int evaluaFuncBool11(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 | char2 ) ^ ( char3 + char4 ) ^ char5;
+	}	
+	
+	// (3,0,0,0)=(|,+,+,+)
+	public int evaluaFuncBool12(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 | char2 ) + ( char3 + char4 ) + char5;
+	}	
+	
+	// (0,1,0,3)=(+,^,+,|)
+	public int evaluaFuncBool13(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) ^ ( char3 + char4 ) | char5;
+	}	
+	
+	// (1,0,0,0)=(^,+,+,+)
+	public int evaluaFuncBool14(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 ^ char2 ) + ( char3 + char4 ) + char5;
+	}	
+	
+	// (2,0,0,0)=(&,+,+,+)
+	public int evaluaFuncBool15(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 & char2 ) + ( char3 + char4 ) + char5;
+	}	
+	
+	// (0,0,1,1)=(+,+,^,^)
+	public int evaluaFuncBool16(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+		return ( char1 + char2 ) + ( char3 ^ char4 ) ^ char5;
+	}	
+	
+// (1,0,1,0)=(^,+,^,+)
+//	public int evaluaFuncBool2(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 ^ char2 ) + ( char3 ^ char4 ) + char5;
+//	}
+	
+	// (0,1,0,3)=(+,^,+,|)
+//	public int evaluaFuncBool5(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 + char2 ) ^ ( char3 + char4 ) | char5;
+//	}
+
+	// (3,0,3,0)=(|,+,|,+)
+//	public int evaluaFuncBool6(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 | char2 ) + ( char3 | char4 ) + char5;
+//	}
+
+	// (0,0,3,0)=(+,+,|,+)
+//	public int evaluaFuncBool7(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 + char2 ) + ( char3 | char4 ) + char5;
+//	}
+
+	// (3,0,1,0)=(|,+,^,+)
+//	public int evaluaFuncBool8(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 | char2 ) + ( char3 ^ char4 ) + char5;
+//	}
+
+	// (0,0,1,3)=(+,+,^,|)
+//	public int evaluaFuncBool10(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 + char2 ) + ( char3 ^ char4 ) | char5;
+//	}
+
+	// (0,0,0,3)=(+,+,+,|)
+//	public int evaluaFuncBool11(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 + char2 ) + ( char3 + char4 ) | char5;
+//	}
+
+	// (3,0,0,1)=(|,+,+,^)
+//	public int evaluaFuncBool12(Integer char1, Integer char2, Integer char3, Integer char4, Integer char5) {
+//		return ( char1 | char2 ) + ( char3 + char4 ) ^ char5;
+//	}
+
+}
--- src/org/hashuniforme/main/Main.java
+++ src/org/hashuniforme/main/Main.java
@@ -8,10 +8,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 import org.hashuniforme.hash.funciones.FuncionHash;
-import org.hashuniforme.hash.funciones.HashIterada;
-import org.hashuniforme.hash.funciones.HashIteradaRandom;
+import org.hashuniforme.hash.funciones.HashIteradaRandomBits;
+import org.hashuniforme.hash.funciones.HashJava;
 import org.hashuniforme.hash.tabla.TablaHash;
 
 /**
@@ -29,7 +30,7 @@ public final class Main {
 		int total = 0;
 		
 		int prime = 1009;
-		FuncionHash funcionHash = new HashIteradaRandom( prime );
+		FuncionHash funcionHash = new HashIteradaRandomBits( prime );
 		TablaHash tablaHash = new TablaHash( funcionHash, prime );
 
 		InputStream istream = Main.class.getResourceAsStream( "/esp.txt" );
@@ -68,16 +69,17 @@ public final class Main {
 		int prime = 1009;
 		int iteraciones = 100;
 		
-		FuncionHash funcionHash = new HashIteradaRandom( prime );
+		//FuncionHash funcionHash = new HashIteradaRandomBits( prime );
 
-		//FuncionHash funcionHash = new HashJava( prime );
+		FuncionHash funcionHash = new HashJava( prime );
 		//FuncionHash funcionHash = new HashPrimo( prime );
 		
 		TablaHash tablaHash = new TablaHash( funcionHash, prime );
 		
 		//String prepend = "AAAAAAAAAAAAAAAAAAAA";
-		String prepend = "ZZZZZZZZZZZZZZZZZZZZ";
+		//String prepend = "ZZZZZZZZZZZZZZZZZZZZ";
 		//String prepend = "MMMMMMMMMMMMMMMMMMMM";
+		String prepend = getRandomBits(20);
 		//System.out.println( "STARTING..." );
 		//System.out.println( "<"+prepend+">-["+prepend.length()+"]" );
 		
@@ -97,4 +99,33 @@ public final class Main {
 		long timeNow = System.currentTimeMillis() - timeIni;
 		System.out.println( "TIME = "+(timeNow/(1000.0*60.0))+" mins, "+(timeNow/(1000.0))+" secs." );
 	}
+	
+	public static String getRandomWord( int size ) {
+		StringBuilder sb = new StringBuilder(); 
+		
+		Random random = new Random();
+		int char1;
+		
+		for( int i=0; i<size; i++ ) {
+			char1 = random.nextInt( (int)'Z'-(int)'A'+1 ) + (int)'A';
+			sb.append( (char)char1 );
+		}
+		
+		return sb.toString();
+	}
+
+	public static String getRandomBits( int size ) {
+		StringBuilder sb = new StringBuilder(); 
+		
+		Random random = new Random();
+		int char1;
+		
+		for( int i=0; i<size; i++ ) {
+			char1 = random.nextInt( Integer.MAX_VALUE );
+			sb.append( (char)char1 );
+		}
+		
+		return sb.toString();
+	}
+	
 }
